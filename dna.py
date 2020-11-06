dbcbbb593642e33d04c3c920f35890578fba63ce
MattSokol79 <maciej.sokol79@Gmail.com>
2020-11-06 14:57:11 +0000

    Task finished

--- dna.py
+++ dna.py
@@ -3,14 +3,15 @@
 class DNA_Parsing:
     def __init__(self):
         pass
-
+    # Function counts the A, C, G, T values within a string
     def sequence_calculator(self):
+        # Asks user to input a DNA sequence to be checked
         dna_sequence = input("Please input a DNA Sequence\n => ").upper()
         a_count = 0
         c_count = 0
         g_count = 0
         t_count = 0
-
+        # If a letter matches any of the required letters, the count for that is increased by 1
         for i in dna_sequence:
             if i == "A":
                 a_count += 1
@@ -22,8 +23,17 @@ class DNA_Parsing:
                 t_count += 1
         output = [a_count, c_count, g_count, t_count]
 
-        return "The A, C, G, T count is:\n --> " + str(output)
+        # the return statement gives a nice output outside of a list
+        print("The A, C G and T Count is:")
+        return " ".join(str(x) for x in output)
 
-test = DNA_Parsing()
 
-print(test.sequence_calculator())
+# Calls an object and runs the process
+def main():
+
+    test = DNA_Parsing()
+
+    print(test.sequence_calculator())
+
+if __name__ == '__main__':
+    main()
